db0015c89797c7aff87106fbcff9334553eaffdd
badstam <164039259+badstam@users.noreply.github.com>
2024-03-19 17:08:12 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Legit Boosts And More","WatchingLegit Boosts And More"];
 
 
 let currentIndex = 0;
